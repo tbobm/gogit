5433fa09e77caea7a120df55bf3366cbe681f4f5
Theo Massard <massar_t@etna-alternance.net>
2017-11-24 16:31:30 +0000

    Base cli, implement initial workflow

---
+++ gitcli.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/urfave/cli"
+	"log"
+	"os"
+)
+
+func main() {
+	app := cli.NewApp()
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "repo",
+			Aliases: []string{"r"},
+			Usage:   "Repository management",
+			Subcommands: []cli.Command{
+				{
+					Name:  "Create",
+					Usage: "create a new repository",
+					Action: func(c *cli.Context) error {
+						fmt.Println("New repository created", c.Args().First())
+						return nil
+					},
+				},
+			},
+		},
+	}
+	log.Println("Gitcli")
+	app.Run(os.Args)
+}
