690961424c7ccf934ddb0adc222d2cf66ec026c0
Theo Massard <massar_t@etna-alternance.net>
2017-11-26 15:03:18 +0100

    Add feature: list repository

--- gitcli.go
+++ gitcli.go
@@ -2,11 +2,58 @@ package main
 
 import (
 	"fmt"
+    "net/http"
 	"github.com/urfave/cli"
-    "github.com/google/go-github"
-	"log"
+    "github.com/google/go-github/github"
+    "golang.org/x/oauth2"
+    "context"
+	//"log"
 	"os"
 )
+
+const maxDisplay int = 10
+
+func createRepo(name string, private bool) error {
+    //client := github.NewClient(nil)
+
+    return nil
+}
+
+
+func listRepos(c *cli.Context) {
+    client := getClient()
+    opt := &github.RepositoryListOptions{
+        Sort: "updated",
+    }
+    repos, resp, err := client.Repositories.List(context.Background(), "tbobm", opt)
+    if err != nil {
+        fmt.Println(err)
+    }
+    fmt.Println(resp.Rate.Remaining)
+    fmt.Println("10 last updated repositories: ")
+    for idx, repo := range repos {
+        fmt.Println(repo.GetName(), ": ", repo.GetURL())
+        if idx == maxDisplay {
+            break
+        }
+    }
+}
+
+
+func getClient() *github.Client {
+    var token string
+    var tc *http.Client
+
+    token = os.Getenv("GIT_TOKEN")
+    if token != "" {
+        ts := oauth2.StaticTokenSource(
+            &oauth2.Token{AccessToken: token},
+        )
+        tc = oauth2.NewClient(context.Background(), ts)
+    }
+
+    return github.NewClient(tc)
+}
 
 func main() {
 	app := cli.NewApp()
@@ -24,10 +71,14 @@ func main() {
 						fmt.Println("New repository created", c.Args().First())
 						return nil
 					},
-				},
+                }, {
+                    Name: "List",
+                    Usage: "list top ten last updated repositories",
+                    Action: listRepos,
+                },
 			},
 		},
 	}
-	log.Println("Gitcli")
 	app.Run(os.Args)
+    //listRepos(client)
 }
